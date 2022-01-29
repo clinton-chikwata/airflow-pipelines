140c80a1804b9b46466c6c4ace5d7a86d23133ff
clinton-chikwata <clinton.chikwata@tmcg.co.ug>
2022-01-30 00:46:43 +0300

    second commit

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -9,13 +9,14 @@ class LoadFactOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                redshift_conn_id="",
-                destination_table="",
+                 redshift_conn_id="",
+                 table="",
+                 load_sql_stmt="",
                  *args, **kwargs):
-
         super(LoadFactOperator, self).__init__(*args, **kwargs)
-        self.redshift_conn_id=redshift_conn_id
-        self.destination_table=destination_table
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.load_sql_stmt = load_sql_stmt
 
     def execute(self, context):
         '''
@@ -23,6 +24,9 @@ class LoadFactOperator(BaseOperator):
         The SQL is contained in the hepler class sql_queries
         '''
 
-        self.log.info(f'Starting to append data to fact table {self.destination_table}')
-        redshift=PostgresHook(postgres_conn_id=self.redshift_conn_id)
-        redshift.run(f'INSERT INTO {self.destination_table} (' + SqlQueries.songplay_table_insert + ' )')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        insert_sql = f"""INSERT INTO {self.table} {self.load_sql_stmt};"""
+
+        self.log.info(f"Loading fact table '{self.table}' into Redshift")
+        redshift.run(insert_sql)
